9fb64c25c168247b8a9d55f672e79c320a59d8d7
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 <Vivekpatil254@gmail.com>
2022-04-25 11:14:24 +0530

    Uc2 X or O from User

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,33 +1,41 @@
-
+import java.util.*;
 public class TicTacToeGame {
-	char arr[] = new char[20];
+	static char arr[] = new char[20];
 	
 	
 	public static void main(String args[])
 	{
-		TicTacToeGame object = new TicTacToeGame();
-		object.assign();
-		object.display();
+		//TicTacToeGame object = new TicTacToeGame();
+		//object.
+		assign();
+		//object.
+		display();
+		allowInput();
 	}
 	
-	public void assign()
+	public static void assign()
 	{
 		for (int i=1;i<=10;i++)
 		{
 			arr[i]=' ';
 		}
 	}
-	public void display()
+	public static void display()
 	{
 		for (int i=1;i<=10;i++)
 		{
 			System.out.println(arr[i]);
-			System.out.println("hi");
 		}
 	}
-	
-	
-	
-	
-
+	public static void allowInput() 
+	{
+		char input;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter the input 'X' or 'O'");
+		do {
+		input = scanner.next().charAt(0);
+		System.out.println(input);
+		}while(input!='X'||input!='O'||input!='x'||input!='o');	
+		
+	}
 }
